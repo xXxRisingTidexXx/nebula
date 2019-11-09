0256d3715b2e39914ff80b115df8152a5659b53a
xXxRisingTidexXx <danylopetrik@gmail.com>
2019-11-09 16:44:53 +0200

    Snapshot redesign

--- nebula/ml/plotting.py
+++ nebula/ml/plotting.py
@@ -1,19 +1,11 @@
 from typing import List
 from pandas import DataFrame
-from nebula.snapshots import Snapshot, Zone
+from nebula.snapshots import Snapshot
 from plotly.graph_objects import Figure, Scattermapbox, Scatter3d
 from plotly.subplots import make_subplots
 
 
-def _calc_color(rate: float) -> str:
-    if rate < 400:
-        return 'blue'
-    if rate < 2000:
-        return 'green'
-    return 'red'
-
-
-def _show_zones(figure: Figure, zones: List[Zone]):
+def _show_zones(figure: Figure):
     figure.add_trace(Scattermapbox(
         mode='markers',
         lon=[30.4],
@@ -23,9 +15,9 @@ def _show_zones(figure: Figure, zones: List[Zone]):
     figure.update_layout(
         margin={'t': 60, 'r': 0, 'b': 10, 'l': 10},
         mapbox={
-            'style': 'stamen-terrain',
-            'center': {'lon': -73.605, 'lat': 45.51},
-            'zoom': 12,
+            'style': 'open-street-map',
+            'center': {'lon': 30.4, 'lat': 50.5},
+            'zoom': 11,
             'layers': [
                 {
                     'type': 'fill',
@@ -35,28 +27,7 @@ def _show_zones(figure: Figure, zones: List[Zone]):
                         'type': 'FeatureCollection',
                         'features': [{
                             'type': 'Feature',
-                            'geometry': {
-                                'type': 'MultiPolygon',
-                                'coordinates': [[[
-                                    [-73.606352888, 45.507489991], [-73.606133883, 45.50687600],
-                                    [-73.605905904, 45.506773980], [-73.603533905, 45.505698946],
-                                    [-73.602475870, 45.506856969], [-73.600031904, 45.505696003],
-                                    [-73.599379992, 45.505389066], [-73.599119902, 45.505632008],
-                                    [-73.598896977, 45.505514039], [-73.598783894, 45.505617001],
-                                    [-73.591308727, 45.516246185], [-73.591380782, 45.516280145],
-                                    [-73.596778656, 45.518690062], [-73.602796770, 45.521348046],
-                                    [-73.612239983, 45.525564037], [-73.612422919, 45.525642061],
-                                    [-73.617229085, 45.527751983], [-73.617279234, 45.527774160],
-                                    [-73.617304713, 45.527741334], [-73.617492052, 45.527498362],
-                                    [-73.617533258, 45.527512253], [-73.618074188, 45.526759105],
-                                    [-73.618271651, 45.526500673], [-73.618446320, 45.526287943],
-                                    [-73.618968507, 45.525698560], [-73.619388002, 45.525216750],
-                                    [-73.619532966, 45.525064183], [-73.619686662, 45.524889290],
-                                    [-73.619787038, 45.524770086], [-73.619925742, 45.524584939],
-                                    [-73.619954486, 45.524557690], [-73.620122362, 45.524377961],
-                                    [-73.620201713, 45.524298907], [-73.620775593, 45.523650879]
-                                ]]]
-                            }
+                            'geometry': {}
                         }]
                     }
                 }
--- nebula/snapshots.py
+++ nebula/snapshots.py
@@ -1,11 +1,22 @@
-from typing import Union, List
-from statistics import mean
-from numpy import ndarray
+from typing import Union, List, Any, Dict
+from numpy import ndarray, mean
 from pandas import DataFrame
-from shapely.geometry import MultiPolygon, Polygon
+from shapely.geometry import MultiPolygon, Polygon, mapping
 from nebula.db.queries import select_flats
 from nebula.ml.clustering import kmeans
 from nebula.ml.spatial import geovoronoi
+
+
+def make_snapshot(
+    locality: str,
+    housing: str,
+    cluster_number: int,
+    boundaries: Union[MultiPolygon, Polygon]
+) -> 'Snapshot':
+    flats = select_flats(locality, housing)
+    labels, centroids = kmeans(flats, cluster_number)
+    polygons, assignments = geovoronoi(centroids[:, [1, 2]], boundaries)
+    return Snapshot(flats, labels, centroids, polygons, assignments)
 
 
 class Snapshot:
@@ -17,25 +28,51 @@ class Snapshot:
         polygons: List[Union[MultiPolygon, Polygon]],
         assignments: List[List[int]]
     ):
-        self.flats = flats.copy()
-        self.flats['zone_id'] = self._get_zones_ids(labels, assignments)
-        self.zones = self._make_zones(centroids, polygons, assignments)
+        self.flats = self._make_flats(flats, labels, assignments)
+        self.sites = self._make_sites(centroids, assignments)
+        self.zones = self._make_zones(polygons)
 
     @staticmethod
-    def _get_zones_ids(labels: ndarray, assignments: List[List[int]]):
+    def _make_flats(
+        flats: DataFrame,
+        labels: ndarray,
+        assignments: List[List[int]]
+    ) -> DataFrame:
+        new_flats = flats.copy()
         mappings = {i: a[0] for a in assignments for i in a}
-        return [mappings[l] for l in labels]
+        new_flats['zone_id'] = [mappings[l] for l in labels]
+        return new_flats
+
+    @staticmethod
+    def _make_sites(
+        centroids: ndarray,
+        assignments: List[List[int]]
+    ) -> DataFrame:
+        return DataFrame(
+            [
+                [
+                    mean(centroids[a, 0]),
+                    centroids[a[0], 1],
+                    centroids[a[0], 2]
+                ]
+                for a in assignments
+            ],
+            columns=['rate', 'longitude', 'latitude']
+        )
 
     @staticmethod
     def _make_zones(
-        centroids: ndarray,
-        polygons: List[Union[MultiPolygon, Polygon]],
-        assignments: List[List[int]]
-    ) -> List['Zone']:
-        rates = centroids[:, [0]]
+        polygons: List[Union[MultiPolygon, Polygon]]
+    ) -> List[Dict[str, Any]]:
         return [
-            Zone(p, mean(rates[i, 0] for i in a))
-            for p, a in zip(polygons, assignments)
+            {
+                'type': 'FeatureCollection',
+                'features': [{
+                    'type': 'Feature',
+                    'geometry': mapping(p)
+                }]
+            }
+            for p in polygons
         ]
 
     def __str__(self) -> str:
@@ -43,42 +80,3 @@ class Snapshot:
 
     def __repr__(self) -> str:
         return f'(\n{self.flats},\n{self.zones}\n)'
-
-
-class Zone:
-    def __init__(self, polygon: Union[MultiPolygon, Polygon], rate: float):
-        self.polygons = self._split_polygon(polygon)
-        self.rate = rate
-
-    def _split_polygon(
-        self,
-        polygon: Union[MultiPolygon, Polygon]
-    ) -> List[DataFrame]:
-        if isinstance(polygon, Polygon):
-            return [self._get_coordinates(polygon)]
-        return [self._get_coordinates(p) for p in polygon]
-
-    @staticmethod
-    def _get_coordinates(polygon: Polygon) -> DataFrame:
-        return DataFrame(
-            polygon.exterior.coords,
-            columns=['longitude', 'latitude']
-        )
-
-    def __str__(self) -> str:
-        return f'(\n{self.polygons},\n{self.rate}\n)'
-
-    def __repr__(self) -> str:
-        return f'(\n{self.polygons},\n{self.rate}\n)'
-
-
-def make_snapshot(
-    locality: str,
-    housing: str,
-    cluster_number: int,
-    boundaries: Union[MultiPolygon, Polygon]
-) -> Snapshot:
-    flats = select_flats(locality, housing)
-    labels, centroids = kmeans(flats, cluster_number)
-    polygons, assignments = geovoronoi(centroids[:, [1, 2]], boundaries)
-    return Snapshot(flats, labels, centroids, polygons, assignments)
